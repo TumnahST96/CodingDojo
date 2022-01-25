57403ba0dcbd13e78518d7057a8a280d896f1923
Tammana Sultana <ts25583@email.vccs.edu>
2022-01-24 20:10:13 -0500

    First

---
+++ Sultana_Tammana_JSPredict.js
@@ -0,0 +1,46 @@
+// hello
+//Dojo
+
+
+//hello
+//result is 15
+
+
+//num is 3
+// result is 18
+
+
+//15
+//10
+//10
+//15
+
+
+
+//15
+//10
+//20
+//15
+
+
+
+//num is 3
+//num is 3
+//result is 16
+
+
+//2
+//5
+//3
+//8
+
+
+//sum is 5
+//sum is 8
+//result is 13
+
+
+//sum is 5
+//sum is 6
+//sum is 8
+//result is 19
