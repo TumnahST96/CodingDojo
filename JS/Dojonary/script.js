8bde83e4535e391bdf74eb3228f4588c8421856e
TumnahST96 <ts25583@email.vccs.edu>
2022-02-01 23:00:14 -0500

    Update script.js

--- JS/Dojonary/script.js
+++ JS/Dojonary/script.js
@@ -9,13 +9,16 @@ function Remover (element){
 var testdiv1 = document.querySelector('.NinSkill .likes')
 var count1 = 0;
 function clickevent1 () {
+    alert("Ninja was liked")
     count1++;
     testdiv1.innerText = count1+" likes"
-
+    
 }
+
 var testdiv = document.querySelector('.NinProg .likes')
 var count = 0;
 function clickevent2 () {
+    alert("Ninja was liked")
     count++;
     testdiv.innerText = count+" likes"
 
