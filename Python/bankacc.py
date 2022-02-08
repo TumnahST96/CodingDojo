54b1dadb3419a76a99fd1316cf352ecac7fa6556
TumnahST96 <ts25583@email.vccs.edu>
2022-02-07 23:56:07 -0500

    more

---
+++ Python/bankacc.py
@@ -0,0 +1,44 @@
+
+import re
+from threading import activeCount
+
+
+class BankAccount:
+    def __init__(self, Fname, Lname, balance, intRate):
+        self.Fname = Fname
+        self.Lname = Lname
+        self.account_balance = 0
+        if balance>0:
+            self.account_balance = balance
+        else:
+            self.account_balance = 0
+        self.intRate = intRate
+    def addInterest(self):
+        # print("hi after")
+        if self.account_balance>0:
+            self.account_balance+=self.account_balance*self.intRate 
+            return self
+    def deposit(self, amount):
+        self.account_balance+=amount
+        self.addInterest()
+        return self
+    def withdraw(self, amount):
+        if self.account_balance>self.account_balance-amount:
+            self.account_balance -=amount
+           
+        else:
+            self.account_balance = self.account_balance
+            print("Not enough balance")
+            
+        return self
+    def display_user_balance(self):
+        
+        print(f"hello Mr. {self.Fname}! Your balance is {self.account_balance}")
+        return self
+
+Tamma = BankAccount("aa", "bb",120, .02)
+
+Tamma.deposit(200).deposit(200).deposit(200).withdraw(300).display_user_balance()
+
+Steve = BankAccount("steve", "bb",120, .02)
+Steve.deposit(200).deposit(200).withdraw(50).withdraw(300).withdraw(20).addInterest().withdraw(300).display_user_balance()
--- Python/chain.py
+++ Python/chain.py
@@ -24,3 +24,47 @@ nahi.make_Deposit(1200).make_withdraw(500).make_withdraw(20).make_withdraw(50).d
 
 genie.make_withdraw(50).display_user_balance()
 nahi.make_Deposit(50).display_user_balance()
+
+
+# class BankAccount:
+#     # ... __init__ goes here
+#     def with_draw(self,amount):
+#         # we can use the static method here to evaluate
+#         # if we can with draw the funds without going negative
+#         if BankAccount.can_withdraw(self.balance,amount):
+#             self.balance -= amount
+#         else:
+#             print("Insufficient Funds")
+#         return self    
+#     # static methods have no access to any attribute
+#     # only to what is passed into it
+#     @staticmethod
+#     def can_withdraw(balance,amount):
+#     	if (balance - amount) < 0:
+# 	    return False
+#         else:
+# 	    return True
+
+
+
+# class BankAccount:
+#     # class attribute
+#     bank_name = "First National Dojo"
+#     all_accounts = []
+#     def __init__(self, int_rate,balance):
+#         self.int_rate = int_rate
+#         self.balance = balance
+#         BankAccount.all_accounts.append(self)
+#     
+#     # class method to change the name of the bank
+#     @classmethod
+#     def change_bank_name(cls,name):
+#         cls.bank_name = name
+#     # class method to get balance of all accounts
+#     @classmethod
+#     def all_balances(cls):
+#         sum = 0
+#         # we use cls to refer to the class
+#         for account in cls.all_accounts:
+#             sum += account.balance
+#         return sum
