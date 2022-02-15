f2e21a65e12a15c0252efb90575086e414057978
TumnahST96 <ts25583@email.vccs.edu>
2022-02-15 14:46:08 -0500

    take info from form

---
+++ Python/FlaskFunda/dojo_fruit_store-master/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)  
+
+@app.route('/')         
+def index():
+    return render_template("index.html")
+
+@app.route('/checkout', methods=['POST'])         
+def checkout():
+    print(request.form)
+    firstName = request.form["first_name"]
+    lastName = request.form["last_name"]
+    StuId =  request.form["student_id"]
+    count = 0
+    apple = request.form["apple"]
+    count+=int (apple)
+    blackberry = request.form["blackberry"]
+    count+= int (blackberry)
+    raspberry =  request.form["raspberry"]
+    count+= int (raspberry)
+    strawberry = request.form["strawberry"]
+    count+= int (strawberry)
+    
+
+    return render_template("checkout.html", firstName = firstName, lastName = lastName, StuId = StuId, apple = apple, blackberry = blackberry, raspberry= raspberry, strawberry=strawberry, count = count)
+
+@app.route('/fruits')         
+def fruits():
+    return render_template("fruits.html")
+
+if __name__=="__main__":   
+    app.run(debug=True)    
--- Python/FlaskFunda/form_test/server.py
+++ Python/FlaskFunda/form_test/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
 app = Flask(__name__)
 # our index route will handle rendering our form
 @app.route('/')
