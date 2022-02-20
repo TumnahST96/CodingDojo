d08d1fa0c95b76240409e734c98fb7be64eed39e
TumnahST96 <ts25583@email.vccs.edu>
2022-02-20 18:26:47 -0500

    doneeee

--- MyStack/server.py
+++ MyStack/server.py
@@ -1,13 +1,24 @@
+
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 from user import  User
 
-@app.route("/")
+#=================Step 1==================
+#This is like main. Program starts here
+#==========================================
+@app.route("/")  #this indicaates function below need to be executed
 def index():
-    # call the get all classmethod to get all friends
+   
+    #=================Step 2==================
+    #This is where code will go to Mode USER 
+    #to get data on whatever we are workin with
+    #==========================================
     user = User.get_all()
     print(user)
+    #======================Step 4=============================
+    #after our model is called, data assigned, we go to next step 
+    #==============================================================
     return render_template("index.html", get_all = user)
 
 
@@ -36,8 +47,55 @@ def create_user():
     return redirect("/")
 
 
+@app.route ("/<int:user_id>")
+def show_user(user_id):
+    data = {
+        "user_id": user_id
+    }
+    info = User.individualShow(data)
+    return render_template("show.html", info = info)
+
+
+#=====================step 8=========================
+# we come back to edit 
+#==============================================
+
+@app.route("/<int:user_id>/edit")
+def edit_user(user_id):
+    data = {
+        "user_id": user_id
+    }
+    info = User.individualShow(data)
+    #=====================step 10=========================
+    # we come back to edit 
+    #==============================================
+    return render_template("edit.html", info = info)
+
+@app.route("/update_user/<int:user_id>", methods=["POST"])
+def update(user_id):
+    data = {
+        #this side matches sql dtaa base == this side matches html declaration 
+        "user_id": user_id,
+        "first_name": request.form["fname"],
+        "last_name" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    User.individualEdit(data)
+    return redirect(f"/{user_id}")
+
+
+@app.route("/<int:user_id>/delete")
+def edit_delete(user_id):
+    data = {
+        "user_id": user_id
+    }
+    info = User.individualDelete(data)
+    return redirect("/")
+    # return render_template("edit.html", info = info)
 
             
 if __name__ == "__main__":
     app.run(debug=True)
 
+#render_template: show me specific html. 
+#redirect --redirects to desired place
--- MyStack/user.py
+++ MyStack/user.py
@@ -1,9 +1,16 @@
 
+from contextlib import redirect_stderr
 import email
+
 from mysqlconnection import connectToMySQL
 
 class User:
     def __init__(self, data):
+         #=================Step 3===========================
+        #This is where code will look at user model
+        #my database has is user and has a tbale user table
+        #in there i have data like first name, last, and stuff
+        #===================================================
         self.id = data['id']
 
         self.first_name = data['first_name']
@@ -13,6 +20,12 @@ class User:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+        #==============================================================
+        #after model is created with all the attributes here in model
+        #its sent back to server.py. but server.py did call get_All class
+        #method, so our function has to go through  get all after init, initial def
+        #==============================================================
+
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM user_tab;"
@@ -21,17 +34,39 @@ class User:
 
         print(results)
         for i in results:
-            uzs.append(cls(i))
+            uzs.append(cls(i))  #cls(i) goes to constructor init and assigns variable in respective data field
 
         print(uzs)
         return uzs
 
-    #create an inser method
+        ##step 3 done and returned to server 
+
+    #create an insert method
     @classmethod
     def save(cls, data):
         query = "INSERT INTO user_tab (first_name, last_name, email, created_At, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s,  NOW(), NOW())"
 
         return connectToMySQL('users').query_db(query, data)
+
+
+    @classmethod 
+    def individualShow(cls, data):
+        query = "SELECT * FROM user_tab WHERE id = %(user_id)s;"
+        results = connectToMySQL('users').query_db(query, data)
+        return cls(results[0])
+
+    #=====================step 9==================
+    # working with edit
+    #==============================================
+    @classmethod
+    def individualEdit(cls, data):
+        query = "UPDATE user_tab set first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(user_id)s;"
+        return connectToMySQL('users').query_db(query, data)
+      
        
-        
+    @classmethod
+    def individualDelete(cls, data):
+        query = "DELETE FROM user_tab WHERE id = %(user_id)s;"
+        results =  connectToMySQL('users').query_db(query, data)
+        return
 
