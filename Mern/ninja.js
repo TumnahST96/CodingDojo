c8b0e57a1d1e0dc68c21af93be3e75ca39feed8e
TumnahST96 <ts25583@email.vccs.edu>
2022-03-08 17:56:28 -0500

    sensei added

--- Mern/ninja.js
+++ Mern/ninja.js
@@ -12,12 +12,25 @@ class Ninja {
     }
 
     showStats() {
-        console.log(`Ninja's name is ${this.name} and speed and strength are ${this.speed}, ${this.strength}`)
+        console.log(`${this.constructor.name}'s name is ${this.name} and speed and strength are ${this.speed}, ${this.strength}`)
         return this
     }
     drinkSake() {
         this.health += 10
         return this
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name, wisdom = 10) {
+        super(name, 200, 10, 10)
+        this.wisdom = wisdom
+
+    }
+
+    speakWisdom() {
+        super.drinkSake()
+        console.log(`${this.name} sensei's health is ${this.health}`)
     }
 }
 
@@ -30,3 +43,10 @@ class Ninja {
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
+
+// example output
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
