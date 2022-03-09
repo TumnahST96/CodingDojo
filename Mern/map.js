b573cb00dfda20112b8d50c183b774837b987962
TumnahST96 <ts25583@email.vccs.edu>
2022-03-08 19:37:37 -0500

    pokeman

---
+++ Mern/map.js
@@ -0,0 +1,6 @@
+const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+const groceryList = groceries.map(item => `<li>${item}</li>`);
+
+
+const values = [1, 2, 3, 4, 5];
+const cubes = values.map(val => val ** 3);
