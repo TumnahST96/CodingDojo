177347037f4bcbb143cb80e88f6ac39242df1a07
TumnahST96 <ts25583@email.vccs.edu>
2022-03-08 23:13:42 -0500

    react js

---
+++ Mern/my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h1 > hello Dojo! < /h1>  <
+        h2 > Things I need to do : < /h2>   <
+        li > Learn React < /li>   <
+        li > Learn Mern < /li>  <
+        li > Eat more sushi < /li>  <
+        li > Buy a car < /li>  < /
+        div >
+    );
+}
+
+export default App;
---
+++ Mern/my-app/src/components/dojo.jsx
@@ -0,0 +1,14 @@
+class App extends Component {    
+        render() {        
+            return (            
+                <form action="/process" method="post">                
+                    <div class="form-group">                    
+                        <label htmlFor="email">Email:</label>                    
+                        <input type="text" id="email" name="username" class="form-control" />                
+                    <div>            
+                </form>        
+            );    
+        }
+    }
+    
+    comsole.log("hey there ")
