ab162bc2d406cd1c7dcec5a0009dcca7fd4a5b0a
TumnahST96 <ts25583@email.vccs.edu>
2022-03-09 23:46:16 -0500

    pixel

---
+++ Mern/pixel/pixel/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Pixel from "./components/Pixel";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>heyyyyy</h1>
+      <Pixel />
+    </div>
+  );
+}
+
+export default App;
