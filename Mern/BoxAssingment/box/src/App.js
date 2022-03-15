9b342c8a3114d1223ddcad44aa3e65a058300cf8
TumnahST96 <ts25583@email.vccs.edu>
2022-03-15 00:37:15 -0400

    map form
    some react strategy

--- Mern/BoxAssingment/box/src/App.js
+++ Mern/BoxAssingment/box/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Place from "./components/Place";
 
 function App() {
+  const [places, setPlaces] = useState([
+    "Mason's Creamery",
+    "Mitchell's Ice Cream",
+    "Margie's Candies",
+  ]);
+
+  const [form, setForm] = useState("");
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const newState = [...places, form];
+    setPlaces(newState);
+    setForm("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Favorite Ice Cream Places</h1>
+      {/* {
+        for(let i =0; i<places.length; i++){
+          <p>{places[i]}</p>
+        }
+      } */}
+
+      <form onSubmit={onSubmitHandler}>
+        <input
+          value={form}
+          onChange={(event) => {
+            setForm(event.target.value);
+          }}
+          type="text"
+          className="w-50 mx auto form-control"
+          placeholder="What's your favorite Ice crreasm place?"
+        />
+
+        <input
+          type="submit"
+          className="btn btn-primary btn-lg d-block mx-auto my-2"
+        />
+      </form>
+
+      {places.map((item, i) => {
+        console.log(`function run ${i}, item: ${item}`);
+        return <Place key={i} name={item} />;
+        // (
+        //   <ul>
+        //     {i} {item}
+        //   </ul>
+        // );
+      })}
     </div>
   );
 }
