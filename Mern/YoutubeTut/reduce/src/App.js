3546c0b42703cd7f8e145722b9601db18d238dab
TumnahST96 <ts25583@email.vccs.edu>
2022-03-14 22:58:15 -0400

    reduce doesn't work

--- Mern/YoutubeTut/reduce/src/App.js
+++ Mern/YoutubeTut/reduce/src/App.js
@@ -14,131 +14,120 @@ const ACTIONS = {
 function reducer(todos, action) {
   switch (action.type) {
     case ACTIONS.ADD_TODO:
-      return [
-        ...todos,
-        newTodo(
-          action.payload.Fname,
-          action.payload.Lname,
-          action.payload.email
-        ),
-      ];
+      if (action.payload.name === "Fname") {
+        if (action.payload.value.length < 2) {
+          return {
+            ...todos,
+            [action.payload.name]: {
+              value: action.payload.value,
+              error: "First name too short",
+            },
+          };
+        }
+        return {
+          ...todos,
+          [action.payload.name]: {
+            value: action.payload.value,
+            error: null,
+          },
+        };
+      } else if (action.payload.name === "Lname") {
+        if (action.payload.value.length < 2) {
+          return {
+            ...todos,
+            [action.payload.name]: {
+              value: action.payload.value,
+              error: "Last name too short",
+            },
+          };
+        }
+        return {
+          ...todos,
+          [action.payload.name]: {
+            value: action.payload.value,
+            error: null,
+          },
+        };
+      } else if (action.payload.name === "email") {
+        if (!validateEmail(action.payload.value)) {
+          return {
+            ...todos,
+            [action.payload.name]: {
+              value: action.payload.value,
+              error: "Invalid email address!",
+            },
+          };
+        }
+      } else {
+        return todos;
+      }
+
+    default:
+      return todos;
   }
 }
 
-function newTodo(valF, valL, valEm) {
-  return {
-    id: Date.now(),
-    Fname: valF,
-    Lname: valL,
-    email: valEm,
-    complete: false,
-  };
+function validateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
+  return false;
 }
+const initialState = {
+  Fname: {
+    value: "",
+    error: null,
+  },
+  Lname: {
+    value: "",
+    error: null,
+  },
+  email: {
+    value: "",
+    error: null,
+  },
+};
 
 export default function App() {
-  const [todos, dispatch] = useReducer(reducer, []);
-  const [Fname, setFName] = useState("");
-  const [Lname, setLname] = useState("");
-  const [email, setEmail] = useState("");
-
-  // function handleChange(e) {
-  //   const { name, value } = e.target;
-  //   dispatch({
-  //     type: name,
-  //     payload: value,
-  //   });
-  // }
+  const [todos, dispatch] = useReducer(reducer, initialState);
+  // const [Fname, setFName] = useState("");
+  // const [Lname, setLname] = useState("");
+  // const [email, setEmail] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
     dispatch({
       type: ACTIONS.ADD_TODO,
-      payload: { Fname: Fname, Lname: Lname, email },
+      payload: e.target,
     });
-    setFName("");
-    setLname("");
-    setEmail("");
   }
 
   console.log(todos);
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div>
           <label>
             <span>First Name:</span>{" "}
-            <input
-              type="text"
-              value={Fname}
-              onChange={(e) => setFName(e.target.value)}
-            />
+            <input type="text" name="Fname" onChange={handleSubmit} />
           </label>
         </div>
 
         <div>
           <label>
             <span>Last Name:</span>{" "}
-            <input
-              type="text"
-              value={Lname}
-              onChange={(e) => setLname(e.target.value)}
-            />
+            <input type="text" name="Lname" onChange={handleSubmit} />
           </label>
         </div>
         <div>
           <label>
             <span>Email:</span>{" "}
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <input type="text" name="email" onChange={handleSubmit} />
           </label>
         </div>
+        <input type="submit" value="Submit the form" />
       </form>
     </div>
   );
 }
-
-// import logo from "./logo.svg";
-// import "./App.css";
-// import React, { useReducer, useState } from "react";
-
-// const ACTIONS = {
-//   ADD_TODO: "add-todo",
-// };
-
-// function reducer(state, action) {
-//   // switch(action.type){
-//   //     case ACTIONS.ADD_TODO
-//   // }
-// }
-
-// export default function App() {
-//   const [state, dispatch] = useReducer(reducer, initialState);
-//   const [name, setName] = useState(" ");
-
-//   function handleChange(e) {
-//     const { name, value } = e.target;
-//     dispatch({
-//       type: name,
-//       payload: value,
-//     });
-//   }
-
-//   return (
-//     <div>
-//       {JSON.stringify(state)}
-//       <div>
-//         <form>
-//           <input
-//             type="text"
-//             value={name}
-//             onChange={(e) => setName(e.target.value)}
-//           />
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
