af433956cd8058f9b53fb7280ae532de27a05b61
TumnahST96 <ts25583@email.vccs.edu>
2022-03-16 19:08:47 -0400

    catching names of all pokies

--- Mern/API/catchpoki/src/App.js
+++ Mern/API/catchpoki/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+  const [pokies, setPokies] = useState([]);
+  //const pokies = []
+  //setPokies= function updates the pokies
+
+  const fetchInfo = () => {
+    console.log("caLLING MEEE");
+
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.results);
+        // console.log(res.data);
+        // console.log(res.data.results);
+
+        setPokies(res.results);
+      })
+      .catch((err) => console.log(err));
+
+    // "https://pokeapi.co/api/v2/pokemon/
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hey there</h1>
+
+      <button onClick={fetchInfo} className="btn btn-primary">
+        Fetch Poki
+      </button>
+
+      <ul>
+        {pokies.map((item, i) => {
+          return <li key={i}>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
