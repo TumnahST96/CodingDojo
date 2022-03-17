831fb40c6c994ecd63230956e05c8a6777802005
TumnahST96 <ts25583@email.vccs.edu>
2022-03-17 02:52:37 -0400

    axiom poki

---
+++ Mern/API/axiompoke/axiompokiprac/src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [pokies, setPokies] = useState([]);
+
+  function handleInput() {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon=807")
+      .then((response) => setPokies(response.data.results));
+    
+  }
+
+  return (
+    <div className="App">
+      <button onClick={handleInput}> Fetch Pokies</button>
+
+      <ul>
+        {pokies.map((item, i) => {
+          return <li key={i}>{item.name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
