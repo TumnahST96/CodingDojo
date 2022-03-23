8e909e08caef432a67bd4af583c0bbadb87b2087
TumnahST96 <ts25583@email.vccs.edu>
2022-03-23 00:59:03 -0400

    CRUD SOMEWHAT COMPLETED

--- Mern/More Mongo/Student/server/controllers/student.controller.js
+++ Mern/More Mongo/Student/server/controllers/student.controller.js
@@ -25,3 +25,35 @@ module.exports.findOne = (req, res) => {
 
 
 }
+
+module.exports.findAllbyState = (req, res) => {
+    Student.findAllbyState({home_state: req.params.home_state})
+    .then(results => res.json(results))
+        .catch(err=>res.status(400).json({message:"that didnt go as planned", err}))
+
+
+}
+
+module.exports.deleteOne = (req, res) => {
+    Student.deleteOne({_id: req.params._id})
+    .then(results => res.json(results))
+        .catch(err=>res.status(400).json({message:"that didnt go as planned", err}))
+
+
+}
+
+module.exports.updateOne = (req, res) => {
+    Student.updateOne({_id: req.params._id}, req.body)
+    .then(results => res.json(results))
+        .catch(err=>res.status(400).json({message:"that didnt go as planned", err}))
+
+
+}
+// module.exports.addBirth = (req, res) => {
+//     Student.updateOne({_id: req.params._id}, 
+//         {$push: {birthday: {month: {req.body.month}} }})
+//     .then(results => res.json(results))
+//         .catch(err=>res.status(400).json({message:"that didnt go as planned", err}))
+
+
+// }
--- Mern/More Mongo/Student/server/routes/students.routes.js
+++ Mern/More Mongo/Student/server/routes/students.routes.js
@@ -2,8 +2,14 @@ const StudentController = require("../controllers/student.controller");
 
 module.exports = app => {
     app.get("/api/test", StudentController.testResponse);
-    app.get("/api/findAll", StudentController.findAll);
+    app.get("/api/student/findAll", StudentController.findAll);
+    app.get("/api/student/findAllbyState/:home_state", StudentController.findAllbyState);
 
-    app.post("/api/create", StudentController.createStudent);
-    app.get("/api/student/:_id", StudentController.findOne)
+    app.post("/api/student/create", StudentController.createStudent);
+    app.get("/api/student/:_id", StudentController.findOne);
+    app.delete("/api/student/:_id", StudentController.deleteOne);
+
+    app.patch("/api/student/:_id", StudentController.updateOne);
+
+    // app.put("/api/student/:_id/birth", StudentController.addBirth);
 }
