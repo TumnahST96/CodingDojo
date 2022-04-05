b0f4139b35f7e929868c5055e696834da631c446
TumnahST96 <ts25583@email.vccs.edu>
2022-04-05 15:57:40 -0400

    Business Side

--- Java/Fundamentals/Cafe.java
+++ Java/Fundamentals/Cafe.java
@@ -3,103 +3,98 @@ import java.util.*;
 import java.util.Scanner;
 
 public class Cafe {
-    
+
     public static void main(String[] args) {
 
         Choices c = new Choices();
         // APP VARIABLES
-        // Lines of text that will appear in the app. 
+        // Lines of text that will appear in the app.
         String generalGreeting = "Welcome to Cafe Java, ";
         String pendingMessage = ", your order will be ready shortly";
         String readyMessage = ", your order is ready";
         String displayTotalMessage = "Your total is $";
         String orderedMessage = " ordered a ";
-        
+
         // Menu variables (add yours below)
         double mochaPrice = 3.5;
         double dripCoffeePrice = 5.5;
         double lattePrice = 5.5;
         double cappuccinoPrice = 5.5;
-        Hashtable <String, Double> menu = new Hashtable<String, Double>();
-        Hashtable <String, Double> customers = new Hashtable<String, Double>();
-        menu.put("mochaPrice", mochaPrice );
-        menu.put("dripCoffeePrice", dripCoffeePrice );
-        menu.put("lattePrice", lattePrice );
-        menu.put("cappuccinoPrice", cappuccinoPrice );
+        Hashtable<String, Double> menu = new Hashtable<String, Double>();
+        Hashtable<String, Double> customers = new Hashtable<String, Double>();
+        menu.put("mochaPrice", mochaPrice);
+        menu.put("dripCoffeePrice", dripCoffeePrice);
+        menu.put("lattePrice", lattePrice);
+        menu.put("cappuccinoPrice", cappuccinoPrice);
 
-
-      
-    
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
-    
 
+        int counter = 0, goal = 55;
+        double DefaultTotal = 0, totalForStore = 0.0;
+        Choices.init(goal, menu);
 
+        System.out.println("-----Add Customer Test-----");
+        Choices.print(customers);
+        System.out.println("Press only 1 if you would like to order");
         Scanner sc = new Scanner(System.in);
         int k = 1;
-        System.out.println("Press only 1 if you would like to order");
         k = sc.nextInt();
-        double DefaultTotal = 0;
-        while(k==1){
-            
-                String b;
-           
-                String name = Choices.getName();
-                Choices.greetings(generalGreeting, name);
-                b = menus(menu);
 
-                double total = Choices.makeChoice(b, menu);
-               
-                    
-                customers.put(name, customers.getOrDefault(name, DefaultTotal)+total);
-                System.out.println("I am here");
+        while (k == 1) {
 
+            String b;
 
-                System.out.println(name + orderedMessage+ " mocha.");
-                Choices.disPlay(displayTotalMessage, customers.get(name), pendingMessage);
-                    
+            String name = Choices.getName();
 
-                isReadyOrder1 = true;
-                Choices.isReady(isReadyOrder1, name, readyMessage);
-                
-              
-            
-            
-                System.out.println("Press 1 to order again, any other key to leave");
-                k = sc.nextInt();
+            Choices.waiting(name, counter);
 
-                print(customers);
+            Choices.greetings(generalGreeting, name);
+
+            b = menus(menu);
+
+            double total = Choices.makeChoice(b, menu);
+            totalForStore = totalForStore + total;
+
+            customers.put(name, customers.getOrDefault(name, DefaultTotal) + total);
+            Choices.print(customers);
+
+            System.out.println(name + orderedMessage + " mocha.");
+            Choices.disPlay(displayTotalMessage, customers.get(name), pendingMessage);
+
+            isReadyOrder1 = true;
+            Choices.isReady(isReadyOrder1, name, readyMessage);
+
+            Choices.OrderTotal(totalForStore);
+
+            if (counter == goal) {
+                System.out.println("We are Sold out");
+            }
+            counter++;
+            System.out.println("Press 1 to order again, any other key to leave");
+            k = sc.nextInt();
+
         }
     }
 
-      
-    public static void print(Hashtable <String, Double> name){
-        Enumeration<String> names = name.keys();
-        while(names.hasMoreElements()){
-            String str = (String) names.nextElement();
-            System.out.println(str+ name.get(str));
-        }
-    }
-    
-    public static String menus (Hashtable <String, Double> menu){
-        System.out.println("Here is Our manu. What would you like?");
+    public static String menus(Hashtable<String, Double> menu) {
+
         Enumeration<String> names = menu.keys();
-        Hashtable<Integer, String> menuIndex = new Hashtable<Integer, String> ();
-        int ct =1;
-        while(names.hasMoreElements()) {
+        Hashtable<Integer, String> menuIndex = new Hashtable<Integer, String>();
+        int ct = 1;
+        while (names.hasMoreElements()) {
             String str = (String) names.nextElement();
-            System.out.println(ct+" " +str + ": " + menu.get(str));
+            System.out.println(ct + " " + str + ": " + menu.get(str));
             menuIndex.put(ct, str);
             ct++;
-         }        
-         System.out.println();
+        }
+        System.out.println();
+        System.out.println("Please Press a Number for Corresponding Order");
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
 
-         Scanner sc = new Scanner(System.in);
-         int b = sc.nextInt();
+        return menuIndex.get(b);
 
-         return menuIndex.get(b);
-
-        
     }
-    
+
 }
--- Java/Fundamentals/Choices.java
+++ Java/Fundamentals/Choices.java
@@ -1,46 +1,88 @@
+import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Choices {
-    public static double makeChoice(String b, Hashtable <String, Double> menu){
+    public static double makeChoice(String b, Hashtable<String, Double> menu) {
 
         double cusTotal = 0.0;
-    
-        cusTotal = cusTotal+ menu.get(b); 
-        
+
+        cusTotal = cusTotal + menu.get(b);
+
         return cusTotal;
-        
+
     }
 
-    public static  void disPlay(String displayTotalMessage, double total,  String pendingMessage) {
-            
-        System.out.println(displayTotalMessage+" "+total+" Thank you for shopping with us"+ pendingMessage);
-        
+    public static void disPlay(String displayTotalMessage, double total, String pendingMessage) {
+
+        System.out.println(displayTotalMessage + " " + total + " Thank you for shopping with us" + pendingMessage);
+
     }
 
-    public static  void greetings(String generalGreeting,  String customer) {
-            
-        System.out.println(generalGreeting+customer);
-        
+    public static void greetings(String generalGreeting, String customer) {
+
+        System.out.println(generalGreeting + customer);
+
     }
 
-    public static String getName(){
+    public static String getName() {
+        System.out.println("Please Enter your name:");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Press 1 for  Cindhuri\n2 for Sam\n3 for Jimmy\nand 4 for Noah ");
-        int a = sc.nextInt();
-        if(a==1) return "Cindhuri";
-        else if(a==2) return "Sam";
-        else if(a==2) return "Jimmy";
-        else return "Noah";
+
+        String a = sc.nextLine();
+        return a;
     }
 
-    public static void isReady(Boolean flag, String customer1, String readyMessage){
-        if(flag){
-            
-                System.out.println(customer1+readyMessage);
-            
+    public static void isReady(Boolean flag, String customer1, String readyMessage) {
+        if (flag) {
+
+            System.out.println(customer1 + readyMessage);
+
         }
-        
+
     }
-    
+
+    public static void print(Hashtable<String, Double> name) {
+        Enumeration<String> names = name.keys();
+        while (names.hasMoreElements()) {
+            String str = (String) names.nextElement();
+            System.out.println(str);
+        }
+    }
+
+    public static void SingleMenu(Hashtable<String, Double> menu) {
+
+        Enumeration<String> names = menu.keys();
+        Hashtable<Integer, String> menuIndex = new Hashtable<Integer, String>();
+        int ct = 1;
+        while (names.hasMoreElements()) {
+            String str = (String) names.nextElement();
+            System.out.println(ct + " " + str + ": " + menu.get(str));
+            menuIndex.put(ct, str);
+            ct++;
+        }
+
+    }
+
+    public static void OrderTotal(double total) {
+        System.out.println("----- Order Total Test -----");
+        System.out.println("Order total: " + total);
+    }
+
+    public static void waiting(String name, int ct) {
+        System.out.println("Hello " + name + "! There are " + ct + " people ahead of you");
+    }
+
+    public static void init(int goal, Hashtable<String, Double> menu) {
+        System.out.println("----- Streak Goal Test -----");
+        System.out.println();
+        System.out.println("Purchases needed by week 10: " + goal);
+        System.out.println();
+
+        System.out.println("-----Display Menu Test-----");
+        SingleMenu(menu);
+        System.out.println();
+
+    }
+
 }
