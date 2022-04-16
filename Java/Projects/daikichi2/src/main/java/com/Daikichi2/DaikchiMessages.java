fd3abdda07b95ed6dd0f0285a73c8024ebb2aa35
TumnahST96 <ts25583@email.vccs.edu>
2022-04-15 20:31:20 -0400

    Update DaikchiMessages.java

--- Java/Projects/daikichi2/src/main/java/com/Daikichi2/DaikchiMessages.java
+++ Java/Projects/daikichi2/src/main/java/com/Daikichi2/DaikchiMessages.java
@@ -1,20 +1,33 @@
 package com.Daikichi2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/daikichi")
 public class DaikchiMessages {
-	@RequestMapping("/")
+//	@RequestMapping("/")
+//	public String first() {
+//		return "Welcome!";
+//	}
+//	@RequestMapping("/today")
+//	public String day() {
+//		return "Today will find luck in all endeavors!";
+//	}
+//	
+//	@RequestMapping("/tomorrow")
+//	public String days() {
+//		return "Tomorrow will find luck in all endeavors!";
+//	}
+	
+	@GetMapping("/travel/{id}")
 	public String first() {
 		return "Welcome!";
 	}
-	@RequestMapping("/today")
-	public String day() {
-		return "Today will find luck in all endeavors!";
-	}
 	
-	@RequestMapping("/tomorrow")
-	public String days() {
-		return "Tomorrow will find luck in all endeavors!";
-	}
+	 @RequestMapping("/m/{track}/{module}/{lesson}")
+	    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
+	    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
+	    }
 }
