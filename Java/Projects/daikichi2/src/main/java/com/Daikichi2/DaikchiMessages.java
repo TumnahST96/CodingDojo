a8d25e3d1360ea747bfa7a52e906b33688f8cc5c
TumnahST96 <ts25583@email.vccs.edu>
2022-04-15 20:49:17 -0400

    redone Daikichi

--- Java/Projects/daikichi2/src/main/java/com/Daikichi2/DaikchiMessages.java
+++ Java/Projects/daikichi2/src/main/java/com/Daikichi2/DaikchiMessages.java
@@ -21,13 +21,20 @@ public class DaikchiMessages {
 //		return "Tomorrow will find luck in all endeavors!";
 //	}
 	
-	@GetMapping("/travel/{id}")
-	public String first() {
-		return "Welcome!";
-	}
-	
-	 @RequestMapping("/m/{track}/{module}/{lesson}")
-	    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
-	    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;
+	@GetMapping("/travel/{city}")
+	public String showLesson(@PathVariable("city") String city){
+	    	return "<h1>Congratulations! You will soon travel to "+ city+"<h1/>";
 	    }
+	
+	@GetMapping("/lotto/{id}")
+	public String Lotto(@PathVariable("id") Integer id){
+		 
+			if(id%2==0) {
+				return "<h1>You will take a grand journey in the near future, but be weary of tempting offers <h1/>";
+			}
+			else return "<h1>You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.<h1/>";
+	    	
+	    	
+	    }
+	
 }
